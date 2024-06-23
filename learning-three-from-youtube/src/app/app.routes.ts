094da6e0e6f8dde8e804c9b1682012db339ecb3a
icecreampoop <pchiku@hotmail.sg>
2024-06-23 09:41:12 +0800

    some progress towards learning three

--- learning-three-from-youtube/src/app/app.routes.ts
+++ learning-three-from-youtube/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FirstObjectComponent } from './components/first-object/first-object.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'homepage', component: HomepageComponent },
+    { path: 'first-object', component: FirstObjectComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' }
+];
---
+++ learning-three-from-youtube/src/app/components/first-object/first-object.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-first-object',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './first-object.component.html',
+  styleUrl: './first-object.component.css'
+})
+export class FirstObjectComponent {
+
+}
